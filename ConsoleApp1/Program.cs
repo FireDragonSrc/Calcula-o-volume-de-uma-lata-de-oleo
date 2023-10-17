ff25b3542e491041862d121f511613cae9924a1c
FireDragonSrc <firedragonsrc@gmail.com>
2023-10-17 10:42:17 -0300

    Commit inicial

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite o raio");
+            float raio = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite a altura");
+            float altura = int.Parse(Console.ReadLine());
+            float volume = 3.14159f * raio * raio * altura;
+            Console.WriteLine(volume);
+            Console.ReadLine();
+        }
+    }
+}
